32dec4b6d48c1e2fea0eba9d2bb111b9ead35a6f
LinelinLove <chrisline.lin@outlook.fr>
2025-04-17 11:15:57 +0200

    add tower placement

--- Assets/Script/Tower.cs
+++ Assets/Script/Tower.cs
@@ -1,39 +1,15 @@
+using UnityEngine;
+
 public class Tower : UnitBase
 {
     public int cost = 50;
     public float fireRate = 1f;
     private float fireCooldown = 0f;
 
-    if (Input.GetMouseButtonDown(0)) // ou touch pour mobile
-    {
-        Vector3 mousePos = Input.mousePosition;
-        Ray ray = Camera.main.ScreenPointToRay(mousePos);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
-        {
-            // Vérifie si le sol est un emplacement valide
-            if (hit.collider.CompareTag("BuildZone")) // Tag de l'herbe par exemple
-            {
-                Tower towerComponent = towerPrefab.GetComponent<Tower>();
-                int towerCost = towerComponent.cost;
-                if (GameManager.Instance.playerMoney >= towerCost)
-                {
-                    GameManager.Instance.playerMoney -= towerCost;
-                    Instantiate(towerPrefab, hit.point, Quaternion.identity);
-                    Debug.Log("Tour placée !");
-                }
-                else
-                {
-                    Debug.Log("Pas assez d'argent !");
-                }
-            }
-        }
-    }
-
     void Update()
     {
         fireCooldown -= Time.deltaTime;
+
         if (fireCooldown <= 0f)
         {
             Transform target = FindNearestTarget("Enemy");
@@ -50,5 +26,4 @@ public class Tower : UnitBase
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
     }
-
 }
