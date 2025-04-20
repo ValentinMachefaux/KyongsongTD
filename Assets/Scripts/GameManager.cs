eac571f4d445691aa1367335598c3f20f7df0a62
Joachim Renevier <renevierjoachim@gmail.com>
2025-04-20 17:45:39 +0200

    a lot of things

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+// using UnityEngine;
+//
+// public class GameManager : MonoBehaviour
+// {
+//     public static GameManager Instance;
+//
+//     public int playerMoney = 100;
+//
+//     void Awake()
+//     {
+//         if (Instance == null)
+//             Instance = this;
+//         else
+//             Destroy(gameObject);
+//     }
+//
+//     public void AddMoney(int amount)
+//     {
+//         playerMoney += amount;
+//         Debug.Log("Money: " + playerMoney);
+//         // Tu peux ici mettre à jour l’UI plus tard
+//     }
+// }
